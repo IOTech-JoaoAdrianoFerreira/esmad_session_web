ed642244a63961e1a83c7386e410a76718a02000
Joao Adriano Teixeira Ferreira <joaoadrianoferreira@iotech.pt>
2018-11-23 17:23:59 +0000

    requests get, put and delete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+function xhrSuccess() {
+    this.callback.apply(this, this.arguments);
+}
+
+function xhrError() {
+    console.error(this.statusText);
+}
+
+function getRecords(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.callback = callback;
+    xhr.arguments = Array.prototype.slice.call(arguments, 2);
+    xhr.onload = xhrSuccess;
+    xhr.onerror = xhrError;
+    xhr.open("GET", url, true);
+    xhr.send(null);
+}
+
+function deleteRecord(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.callback = callback;
+    xhr.arguments = Array.prototype.slice.call(arguments, 2);
+    xhr.onload = xhrSuccess;
+    xhr.onerror = xhrError;
+    xhr.open("DELETE", url, true);
+    xhr.send(null);
+}
+
+function updateRecord(url, value, callback) {
+    var data = {};
+    data.value = value;
+    var json = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+    xhr.callback = callback;
+    xhr.arguments = Array.prototype.slice.call(arguments, 2);
+    xhr.onload = xhrSuccess;
+    xhr.onerror = xhrError;
+    xhr.open("PUT", url, true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(json);
+}
+
 function createChart() {
     let json = JSON.parse(this.responseText);
     let labels = [];
@@ -6,8 +48,8 @@ function createChart() {
         labels.push(element.timestamp);
         data.push(element.value);
     })
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
+    var ctx = document.getElementById('recordChart').getContext('2d');
+    var recordChart = new Chart(ctx, {
         type: 'line',
         options: {
             scales: {
@@ -27,24 +69,6 @@ function createChart() {
     })
 }
 
-function xhrSuccess() {
-    this.callback.apply(this, this.arguments);
-}
-
-function xhrError() {
-    console.error(this.statusText);
-}
-
-function getRecords(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.callback = callback;
-    xhr.arguments = Array.prototype.slice.call(arguments, 2);
-    xhr.onload = xhrSuccess;
-    xhr.onerror = xhrError;
-    xhr.open("GET", url, true);
-    xhr.send(null);
-}
-
 function createTable() {
     let htmlContent = ""
     let records = JSON.parse(this.responseText);
@@ -53,7 +77,7 @@ function createTable() {
         htmlContent = htmlContent + " <th scope='row'>" + record._id + "</th>";
         htmlContent = htmlContent + " <td>" + record.timestamp + "</td>";
         htmlContent = htmlContent + " <td>" + record.value + "</td>";
-        htmlContent = htmlContent + " <td> <button type='button' class='btn btn-outline-dark' data-toggle='modal' data-target='#editRecord'>Edit</button><button type='button' class='btn btn-outline-danger' data-toggle='modal' data-target='#removeRecord'>Remove</button></td>";
+        htmlContent = htmlContent + " <td> <button type='button' class='btn btn-outline-dark' onclick='orderToUpdateRecord(\"" + record._id + "\", \"" + record.value + "\")'>Edit</button><button type='button' class='btn btn-outline-danger' onclick='orderToDeleteRecord(\"" + record._id + "\")'>Remove</button></td>";
         htmlContent = htmlContent + "<tr>";
         htmlContent = htmlContent + "<tr>";
         htmlContent = htmlContent + "<tr>";
@@ -68,3 +92,36 @@ function orderToCreateTable() {
 function orderToCreateChart() {
     getRecords("http://localhost:3000/records", createChart);
 }
+
+function orderToDeleteRecord(id) {
+    document.getElementById("idToRemove").innerHTML = id;
+    $('#removeRecord').modal()
+}
+
+function orderToUpdateRecord(id, value) {
+    document.getElementById("idToUpdate").innerHTML = id;
+    document.getElementById("newRecordValue").value = value;
+    $('#editRecord').modal()
+}
+
+function alertDelete() {
+    alert("Record Removed");
+    location.reload();
+}
+
+function alertUpdate() {
+    alert("Record Updated");
+    location.reload();
+}
+
+function saveRecord() {
+    value = document.getElementById("newRecordValue").value;
+    id = document.getElementById("idToUpdate").innerHTML;
+    updateRecord("http://localhost:3000/records/" + id, value, alertUpdate);
+}
+
+function dropRecord() {
+    id = document.getElementById("idToRemove").innerHTML;
+    deleteRecord("http://localhost:3000/records/" + id, alertDelete);
+}
+
